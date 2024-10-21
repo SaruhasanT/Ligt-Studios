2ec558502608f40647520b40c945fdc29a8548b3
Saruhasan <hasansaru2000@gmail.com>
2024-10-21 13:19:01 +0530

    UI updated!

--- main.js
+++ main.js
@@ -1,27 +1,34 @@
-const wrapper = document.querySelector(".hero .wrapper")
+const wrapper = document.querySelector(".hero .wrapper");
 
-window.addEventListener("scroll",()=>{
-    const scrolled = window.scrollY
-    if(scrolled <= 300){
-        wrapper.style.transform = `rotateX(${-scrolled/3}deg)`
-    }
-})
+window.addEventListener("scroll", () => {
+  const scrolled = window.scrollY;
+  if (scrolled <= 300) {
+    wrapper.style.transform = `rotateX(${-scrolled / 3}deg)`;
+  }
+});
 const options = {
-    root: document.querySelector("body"),
-    rootMargin: "0px",
-    threshold: 0.2,
-  };
-const observer = new IntersectionObserver(entries=>{
-    entries.forEach(entry=>{
-        if(entry.isIntersecting){
-            entry.target.classList.add("active");
-        }else{
-            entry.target.classList.remove("active");
-        }
-    })
-})
+  root: document.querySelector("body"),
+  rootMargin: "0px",
+  threshold: 0.2,
+};
+const observer = new IntersectionObserver(
+  (entries) => {
+    entries.forEach((entry) => {
+      if (entry.isIntersecting) {
+        entry.target.classList.add("active");
+      } else {
+        entry.target.classList.remove("active");
+      }
+    });
+  },
+  {
+    threshold: 1,
+  }
+);
 
-const elements = document.querySelectorAll(".service .description, .service img");
-elements.forEach(el=>{
-    observer.observe(el)
-})
+const elements = document.querySelectorAll(
+  ".service .description, .service img"
+);
+elements.forEach((el) => {
+  observer.observe(el);
+});
