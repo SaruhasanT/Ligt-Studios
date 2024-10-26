e5bb571321c2f7302b4603752e1d3521b1282e1c
Saruhasan <hasansaru2000@gmail.com>
2024-10-26 19:02:03 +0530

    changed

--- main.js
+++ main.js
@@ -1,9 +1,16 @@
 const wrapper = document.querySelector(".hero .wrapper");
-
+const header = document.querySelector("header");
 window.addEventListener("scroll", () => {
   const scrolled = window.scrollY;
   if (scrolled <= 300) {
     wrapper.style.transform = `rotateX(${-scrolled / 3}deg)`;
+  }
+  if (scrolled > 100) {
+    header.classList.add("below");
+    document.querySelector(".hero").style.top = "100px";
+  } else {
+    header.classList.remove("below");
+    document.querySelector(".hero").style.top = "0";
   }
 });
 const options = {
